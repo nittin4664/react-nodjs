f8ee4cff4206cb88eb7dda05ad93296001389054
Nitin <nitinku@thoughtworks.com>
2016-07-03 17:46:22 +0000

    Ignoring node modules and building RPM

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        easy_rpm: {
+            options: {
+                name: "post-node-react",
+                version: "1.0.0",
+                release: 1,
+                buildArch: ""
+            },
+            release: {
+                files: [
+                    {src: "node_modules/**", dest: "/usr/lib/post-node-react"},
+                    {src: "resources/**", dest: "/usr/lib/post-node-react"},
+                    {src: "server.js", dest: "/usr/lib/post-node-react"}
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-easy-rpm");
+    // Default task(s).
+    grunt.registerTask('default', ['easy_rpm']);
+
+};
