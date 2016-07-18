6b09388a9e6a8d74e3baf5368a1e759bf38009cf
Nitin <nittin4664@gmail.com>
2016-07-18 23:24:02 +0530

    hoarding are read from the database if it exist

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-var express = require('express'),
+var db = require('./resources/js/database.js'),
+    express = require('express'),
     app = express();
-var hoardings = [{"name": "koramangala", "role": "near wipro signal"},
-                 {"name": "koramangala", "role": "8th main"},
-                 {"name": "koramangala", "role": "BDA Complex"},
-                 {"name": "koramangala", "role": "Sathyas bar"},
-                 {"name": "koramangala", "role": "Opp ThoughWorks office"},
-                 {"name": "koramangala", "role": "opp CCD"},
-                 {"name": "Indiranagar", "role": "12th main"}]
+
 app.use('/', express.static(__dirname + '/resources'));
 
 app.get('/hoardings', function(res, res) {
-  res.send(hoardings);
+  db.query("SELECT * from hoarding;").then(function(d) {
+    res.send(d);});
 });
 
 app.set('port', process.env.PORT || 3000);
